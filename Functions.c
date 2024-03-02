a9c9699c1ce8de2c1929db447bafb44c3d68312a
Bartlomiej Drapinski <g00428194@atu.ie>
2024-03-02 19:05:54 +0000

    Working on the random generator

--- Functions.c
+++ Functions.c
@@ -1,11 +1,29 @@
-//test
+//Bartek is doing the random function generator and a function to check if the player has won
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
 
-    printf("Test");
+    RandGen();
 
     return 0;
+}
+
+
+int RandGen() {
+     
+    srand(time(NULL));
+
+    char result = (rand() % 2 == 0) ? 'X' : 'O';
+    printf("Random option is: %c\n", result);
+
+    return 0;
+}
+
+int WinChecker() {
+
+
 }
 
