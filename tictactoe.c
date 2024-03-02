8fa53bfce1ee651ff14357bfb15c0eaa6030c31c
g00286398@atu.ie <g00286398@atu.ie>
2024-03-02 13:51:25 +0000

    added comments to work done .

--- tictactoe.c
+++ tictactoe.c
@@ -6,24 +6,25 @@
 #include <math.h>
 #include <stdio.h>
 
-void printTiles(char tiles[][3], int ROW, int COL);
+void printTiles(char tiles[][3], int ROW,
+                int COL); // function prototype to print array
 
 int main() {
   const int ROW = 3;
   const int COL = 3;
-  char tiles[ROW][COL];
+  char tiles[ROW][COL]; // setting array to 3x3 grid using const ints
   int i, j;
 
-  for (i = 0; i < ROW; i++) {
+  for (i = 0; i < ROW; i++) { // sample code to populate grid.
     for (j = 0; j < COL; j++) {
       tiles[i][j] = 'x';
     }
   }
-  printTiles(tiles, ROW, COL);
+  printTiles(tiles, ROW, COL); // calling print function
   return 0;
 }
 void printTiles(char tiles[][3], int ROW, int COL) {
-  printf("+---+---+---+\n");
+  printf("+---+---+---+\n"); // printing out custom display for array .
   printf("| %c | %c | %c |\n", tiles[0][0], tiles[0][1], tiles[0][2]);
   printf("+---+---+---+\n");
   printf("| %c | %c | %c |\n", tiles[1][0], tiles[1][1], tiles[1][2]);
