09f6044ed6e35e4152455d46eae8566ba30de7bf
Simon <g00286398@atu.ie>
2024-03-07 10:40:12 +0000

    Added print grid function as well as loop to decide whos go it is. set this to maximum number of board pieces before someone must win (9)

--- tictactoe.c
+++ tictactoe.c
@@ -1,26 +1,40 @@
-/* Group Project - Tic Tac Toe
- * Simon Hurney
- * 29/02/24
- */
 
 #include <math.h>
 #include <stdio.h>
 
-int main() {
+void printTiles(char tiles[][3], int ROW,
+                int COL); // function prototype to print array
 
-  char array[3][3];
+int main() {
+  const int ROW = 3;
+  const int COL = 3;
+  char tiles[ROW][COL]; // setting array to 3x3 grid using const ints
   int i, j;
 
-  for (i = 0; i < 3; i++) {
-    for (j = 0; j < 3; j++) {
-      array[i][j] = 'x';
+  for (i = 0; i < 9;
+       i++) { // loop to the max number of goes before someone must win
+    if (i % 2 == 0) {
+      // Playergo;
+    } else {
+      // cpu go;
     }
-  }
-  printf("| %c | %c | %c |\n", array[0][0], array[0][1], array[0][2]);
-  printf("+---+---+---+\n");
-  printf("| %c | %c | %c |\n", array[1][0], array[1][1], array[1][2]);
-  printf("+---+---+---+\n");
-  printf("| %c | %c | %c |\n", array[2][0], array[2][1], array[2][2]);
 
+    for (i = 0; i < ROW; i++) { // sample code to populate grid.
+      for (j = 0; j < COL; j++) {
+        tiles[i][j] = 'x';
+      }
+    }
+    printTiles(tiles, ROW, COL); // calling print function
+  }
   return 0;
+}
+
+void printTiles(char tiles[][3], int ROW, int COL) {
+  printf("+---+---+---+\n"); // printing out custom display for array .
+  printf("| %c | %c | %c |\n", tiles[0][0], tiles[0][1], tiles[0][2]);
+  printf("+---+---+---+\n");
+  printf("| %c | %c | %c |\n", tiles[1][0], tiles[1][1], tiles[1][2]);
+  printf("+---+---+---+\n");
+  printf("| %c | %c | %c |\n", tiles[2][0], tiles[2][1], tiles[2][2]);
+  printf("+---+---+---+\n");
 }
